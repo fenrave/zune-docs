7b8bfbdc3ee9177948bdecca2aac0fcbcf502dc2
Sythivo <69376875+Sythivo@users.noreply.github.com>
2025-09-29 00:19:31 -0400

    `0.5.0` (#3)
    API docs, guides and release notes

---
+++ src/content/docs/v0.5.0/api/_meta.js
@@ -0,0 +1,24 @@
+export default {
+  globals: "<globals>",
+  "---": {
+    "type": "separator"
+  },
+  fs: "fs",
+  process: "process",
+  testing: "testing",
+  task: "task",
+  net: "net",
+  luau: "luau",
+  serde: "serde",
+  io: "io",
+  crypto: "crypto",
+  regex: "regex",
+  time: "time",
+  ffi: "ffi",
+  sqlite: "sqlite",
+  require: "require",
+  random: "random",
+  thread: "thread",
+  mem: "mem",
+  platform: "platform",
+}
---
+++ src/content/docs/v0.5.0/commands/_meta.js
@@ -0,0 +1,17 @@
+export default {
+  index: "Introduction",
+  "---": {
+    "type": "separator"
+  },
+  run: "run",
+  test: "test",
+  eval: "eval",
+  debug: "debug",
+  bundle: "bundle",
+  setup: "setup",
+  repl: "repl",
+  init: "init",
+  luau: "luau",
+  help: "help",
+  info: "info",
+}
